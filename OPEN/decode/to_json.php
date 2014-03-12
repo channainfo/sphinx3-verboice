d79cc62dd78932e7bce0593b5def08f5ca81d24d
Channa Ly <channa.info@gmail.com>
2014-03-12 08:46:13 +0700

    cleaning script in decode

--- OPEN/decode/to_json.php
+++ OPEN/decode/to_json.php
@@ -8,17 +8,47 @@
  $lines =  explode("\n", $content);
  
  $skip = 6;
-
+ $limit = count($lines)-2; // count($lines)
  $results = array();
- for($i=$skip; $n= count($lines), $i< $n-2; $i++ ) {
- 	$results[] = parse_line($lines[$i]);
- }
  
- echo json_encode(array("results" => $results, "errors" => $error));
+ $k = 1 ;
+ for($i=$skip; $i< $limit; $i++ ) {
+ 	$results[] = parse_line($lines[$i]);
+    $k++;
+    if($k >3)
+      break;
+ }
 
+#convert the confidence result into percentage
+$total=0;
+$arrlength=count($results);
 
- function parse_line($line) {
+for($x=0;$x<$arrlength;$x++) { 
+	$total+=$results[$x]['confidence'];
+}
+
+for($x=0;$x<$arrlength;$x++) {
+	$results[$x]['confidence'] = number_format(1-($results[$x]['confidence']/$total),3)*100;
+}
+ 
+echo json_encode(array("results" => $results, "errors" => $error));
+
+function parse_line($line) {
  	$items = explode(" ", $line);
- 	return array("result" => $items[17], "confidence" => $items[3]);
+    $result = $items[17] ;
+    $index = 17;
+    while($result == "<sil>") {
+    	if($index < count($items) ) {
+	    	$index = $index + 4;
+	    	$result = $items[$index];
+    	}
+    	else
+    		break;
+    }
+ 	return array("result" => $result , "confidence" => $items[3]);
+ }
+
+ function translate_result($result){
+ 	return $result;
  }
 
